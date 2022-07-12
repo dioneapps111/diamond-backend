d29efee0fa82b204a2e085e679302a4d9f270bf0
vinusmavani <dioneapps111@gmail.com>
2022-07-12 11:56:40 +0530

    purchase entry changes

--- Controllers/pur_saleController.js
+++ Controllers/pur_saleController.js
@@ -22,12 +22,28 @@ exports.pur_saleEntry = async function (req, res, next) {
         //     p_r_type: req.body.p_r_type,
         //     adat: req.body.adat,
         //     adat_amt: req.body.adat_amt,
-        // };\
-        let data = req.body;
-        let addData = await pur_sale.create(data);
+        // };
+        // let data = req.body;
+        let addData = await pur_sale.create(req.body);
         res.status(200).json({
             status: "200",
-            addData: data,
+            addData,
+        });
+    } catch (err) {
+        res.status(200).json({
+            status: "500",
+            message: err.message,
+        });
+    }
+};
+
+exports.get_pur_saleEntry = async function (req, res, next) {
+    try {
+        // let data = req.body;
+        let Data = await pur_sale.find({ inv_no: req.body });
+        res.status(200).json({
+            status: "200",
+            data: Data[0],
         });
     } catch (err) {
         res.status(200).json({
--- routes/pur_saleEntry.js
+++ routes/pur_saleEntry.js
@@ -4,5 +4,6 @@ const AuthController = require("../Controllers/AuthController");
 var router = express.Router();
 
 router.post("/pur_saleEntry", AuthController.protectGlobal, pur_saleController.pur_saleEntry);
+router.get("/pur_saleEntry", AuthController.protectGlobal, pur_saleController.get_pur_saleEntry);
 
 module.exports = router;
